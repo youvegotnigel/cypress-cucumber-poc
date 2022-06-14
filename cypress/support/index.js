d07f50f063bcd29e300c76c5d877f71d4a3cd80c
Nigel Mulholland <nigeldave19@gmail.com>
2022-06-14 13:37:01 +0530

    install xpath plugin

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,6 +19,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+require('cypress-xpath')
 
 afterEach(() => {
     const screenshotsFolder = Cypress.config("screenshotsFolder");
