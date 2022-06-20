961af21211f38080be5b0ffd561eefc7b9816870
Nigel Mulholland <nigeldave19@gmail.com>
2022-06-20 13:00:01 +0530

    allure setup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,10 @@
  */
 // eslint-disable-next-line no-unused-vars
 const cucumber = require('cypress-cucumber-preprocessor').default
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
+  allureWriter(on, config);
+  return config;
 }
